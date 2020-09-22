4d52ab6ab4bc92ade30f4d27ac522bc66e3717a1
JeongaHan <ab012121@naver.com>
2020-09-22 23:36:53 +0900

    정규식

--- basic_programming/regex.py
+++ basic_programming/regex.py
@@ -1,3 +1,32 @@
 import re
 
-p = re.compile('ab*')
+p = re.compile('[a-z]+')
+
+m = p.match("python")
+print(m)
+
+m = p.match("3 python")
+print(m)
+
+m = p.search("python")
+print(m)
+
+m = p.search("3 python")
+print(m)
+
+result = p.findall("life is too short")
+print(result)
+
+result = p.finditer("life is too short")
+print(result)
+for r in result:
+    print(r)
+
+m = p.match("python")
+print(m.group(), m.start(), m.end(), m.span())
+
+m = p.search("3 python")
+print(m.group(), m.start(), m.end(), m.span())
+
+m = re.match('[a-z]+','python')
+print(m.group())
