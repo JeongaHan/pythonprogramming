1543d0a5e90e1c669b4167f0d9c9369f96d6f953
JeongaHan <ab012121@naver.com>
2020-09-24 23:31:58 +0900

    x만큼 간격이 있는 n개의 숫자

---
+++ programmers/xterm.py
@@ -0,0 +1,13 @@
+def solution(x, n):
+    answer = []
+    temp = 0
+    for i in range(n):
+        temp += x
+        answer.append(temp)
+    return answer
+
+
+
+print(solution(2,5))
+print(solution(4,3))
+print(solution(-4,2))
