562c41cf0f932ba459f398bea0c60cd698c74016
JeongaHan <ab012121@naver.com>
2020-09-26 00:18:45 +0900

    모의고사

--- programmers/mock_test.py
+++ programmers/mock_test.py
@@ -1,15 +1,24 @@
 def solution(answers):
-    answer = [0,0,0]
+    score = [0, 0, 0]
+    answer = []
     student = [[1, 2, 3, 4, 5], [2, 1, 2, 3, 2, 4, 2, 5], [3, 3, 1, 1, 2, 2, 4, 4, 5, 5]]
     for i in range(len(answers)):
-        if student[0][i%len(student[0])] == answers[i]:
-            answer[0] += 1
+        if student[0][i % len(student[0])] == answers[i]:
+            score[0] += 1
         if student[1][i % len(student[1])] == answers[i]:
-            answer[1] += 1
+            score[1] += 1
         if student[2][i % len(student[2])] == answers[i]:
-            answer[2] += 1
+            score[2] += 1
 
-
+    if score[0] == score[1] & score[1] == score[2]:
+        return [1, 2, 3]
+    tempmax = max(score)
+    if score[0] == tempmax:
+        answer.append(1)
+    if score[1] == tempmax:
+        answer.append(2)
+    if score[2] == tempmax:
+        answer.append(3)
     return answer
 
 
