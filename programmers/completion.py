b508e31e03bb329852da1c05bdb97e6a8cf52d0a
JeongaHan <ab012121@naver.com>
2020-09-30 23:00:39 +0900

    완주하지 못 한 선수

---
+++ programmers/completion.py
@@ -0,0 +1,11 @@
+def solution(participant, completion):
+    participant.sort()
+    completion.sort()
+    for i in range(len(completion)):
+        if participant[i] != completion[i]:
+            return participant[i]
+    return participant[len(participant)-1]
+
+participant = ['mislav', 'stanko', 'mislav', 'ana']
+completion = ['stanko', 'ana', 'mislav']
+print(solution(participant,completion))
