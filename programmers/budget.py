d3af9497d6cf4f23a8e3eb978d40cc972f85210c
JeongaHan <ab012121@naver.com>
2020-10-01 23:08:08 +0900

    예산

---
+++ programmers/budget.py
@@ -0,0 +1,14 @@
+def solution(d, budget):
+    answer = 0
+    d.sort()
+    for i in range(len(d)):
+        budget -= d[i]
+        if budget < 0:
+            break
+        answer += 1
+    return answer
+
+
+d = [1, 3, 2, 5, 4]
+budget = 9
+print(solution(d, budget))
