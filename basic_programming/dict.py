cc352ed9afcdccfb6e6bbc268f69d4e045513dbb
JeongaHan <ab012121@naver.com>
2020-10-05 23:28:42 +0900

    딕셔너리 자료형

---
+++ basic_programming/dict.py
@@ -0,0 +1,20 @@
+dic = {'name': 'pey', 'phone': '0119993323', 'birth': '1118'}
+a = {1:'hi'}
+a = {'a':[1,2,3]}
+
+a = {1:'a'}
+a[2] = 'b'
+print(a)
+
+a['name'] = 'pey'
+print(a)
+
+a[3] = [1,2,3]
+print(a)
+
+del a[1]
+print(a)
+
+grade = {'pey':10, 'julliet':99}
+print(grade['pey'])
+print(grade['julliet'])
