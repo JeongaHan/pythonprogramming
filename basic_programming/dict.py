d6ba0ecf369b92fb3c396cf671fb222ada1d6d23
JeongaHan <ab012121@naver.com>
2020-10-06 23:54:54 +0900

    dictionary 자료형

--- basic_programming/dict.py
+++ basic_programming/dict.py
@@ -1,20 +1,28 @@
 dic = {'name': 'pey', 'phone': '0119993323', 'birth': '1118'}
-a = {1:'hi'}
-a = {'a':[1,2,3]}
+a = {1: 'hi'}
+a = {'a': [1, 2, 3]}
 
-a = {1:'a'}
+a = {1: 'a'}
 a[2] = 'b'
 print(a)
 
 a['name'] = 'pey'
 print(a)
 
-a[3] = [1,2,3]
+a[3] = [1, 2, 3]
 print(a)
 
 del a[1]
 print(a)
 
-grade = {'pey':10, 'julliet':99}
+grade = {'pey': 10, 'julliet': 99}
 print(grade['pey'])
 print(grade['julliet'])
+
+a = {1: 'a', 2: 'b'}
+print(a[1])
+print(a[2])
+
+a = {'a': 1, 'b': 2}
+print(a['a'])
+print(a['b'])
