0d2e7655ceb89151fdf7c33e3f7349a1c0843856
JeongaHan <ab012121@naver.com>
2020-10-26 01:03:00 +0900

    분해합

---
+++ algorithm/d_sum.py
@@ -0,0 +1,15 @@
+m = int(input())
+constructor = -1
+for n in range(0, m):
+    sum = n
+    temp = n
+    while temp > 0:
+        sum = sum + temp % 10
+        temp = temp // 10
+    if sum == m:
+        print(n)
+        constructor = n
+        break
+if constructor == -1:
+    print(0)
+
