2aa2fbf7689b3451817f46fc34667fb6f87b7dd7
JeongaHan <ab012121@naver.com>
2020-10-29 00:04:23 +0900

    수 정렬하기 quick sort

--- algorithm/수 정렬하기2.py
+++ algorithm/수 정렬하기2.py
@@ -2,6 +2,20 @@ n = int(input())
 arr = list()
 for i in range(n):
     arr.append(int(input()))
-arr.sort()
-for item in arr:
-    print(item)
+
+
+def quick_sort(array):
+    if len(array) <= 1: return array
+    pivot = array[len(array) // 2]
+    left, equal_arr, right = [], [], []
+    for i in array:
+        if i < pivot:
+            left.append(i)
+        elif i > pivot:
+            right.append(i)
+        else:
+            equal_arr.append(i)
+    return quick_sort(left) + equal_arr + quick_sort(right)
+
+
+print(quick_sort(arr))
