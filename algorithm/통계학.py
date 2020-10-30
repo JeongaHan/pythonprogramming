2fd9cfa235e38171405992d73adfb17e6b63833d
JeongaHan <ab012121@naver.com>
2020-10-30 22:44:00 +0900

    통계학

--- algorithm/통계학.py
+++ algorithm/통계학.py
@@ -11,14 +11,13 @@ print(arr[n // 2])
 
 arrdict = dict()
 for item in arr:
-    arrdict[item] = 0
-for item in arr:
-    if arrdict[item] > 1:
-        arrdict[item] += 1
-    else:
+    if arrdict.get(item) is None:
         arrdict[item] = 1
-if max(arrdict.values()) == 1:
+    else:
+        arrdict[item] += 1
+if max(arrdict.values()) == 1 and len(arr)>1:
     print(arr[1])
-arrdict2 = {v: k for k, v in arrdict.items()}
-print(arrdict)
-print(arrdict2)
+else:
+    print([k for k, v in arrdict.items() if v == max(arrdict.values())][0])
+
+print(max(arr)-min(arr))
