52231057c3dc8df0c4e98b461482b15d45674800
JeongaHan <ab012121@naver.com>
2020-11-03 23:30:34 +0900

    수 정렬

--- algorithm/수 정렬하기2.py
+++ algorithm/수 정렬하기2.py
@@ -1,21 +1,10 @@
-n = int(input())
+import sys
+
+
+n = int(sys.stdin.readline())
 arr = list()
 for i in range(n):
-    arr.append(int(input()))
-
-
-def quick_sort(array):
-    if len(array) <= 1: return array
-    pivot = array[len(array) // 2]
-    left, equal_arr, right = [], [], []
-    for i in array:
-        if i < pivot:
-            left.append(i)
-        elif i > pivot:
-            right.append(i)
-        else:
-            equal_arr.append(i)
-    return quick_sort(left) + equal_arr + quick_sort(right)
-
-
-print(quick_sort(arr))
+    arr.append(int(sys.stdin.readline()))
+arr.sort()
+for i in arr:
+    print(i)
