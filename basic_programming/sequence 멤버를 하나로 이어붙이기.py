32133621b558f60c2024f8271d2d276e73a0439e
JeongaHan <ab012121@naver.com>
2020-11-08 16:59:45 +0900

    sequence 멤버를 하나로 이어붙이기

---
+++ basic_programming/sequence 멤버를 하나로 이어붙이기.py
@@ -0,0 +1,11 @@
+# def solution(mylist):
+#     answer = ''
+#     for item in mylist:
+#         answer += item
+#     return answer
+
+def solution(mylist):
+    return ''.join(mylist)
+
+
+print(solution(['1', '100', '33']))
